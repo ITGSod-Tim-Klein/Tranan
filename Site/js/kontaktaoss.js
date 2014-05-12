63492683270f246b8fe863a4767bb5b3ce472f32
Lambi <tim.kleinbergman@elev.it-gymnasiet.se>
2014-05-12 14:53:57 +0200

    Fixed javascript on city selection drop down

---
+++ Site/js/kontaktaoss.js
@@ -0,0 +1,27 @@
+
+
+//function change(value){
+//    if (value == "sthlm") {
+//        document.getElementById("text1").outerHTML="556 517 39";
+//        document.getElementById("text2").outerHTML="556 518 91";
+//        document.getElementById("text3").outerHTML="070 331 59 56";
+//    }
+//    else if (value == "gbg") {
+//        document.getElementById("text1").outerHTML="556 517 24";
+//        document.getElementById("text2").outerHTML="556 518 11";
+//        document.getElementById("text3").outerHTML="070 331 32 56";
+//    }
+//    else if (value == "kiruna") {
+//        document.getElementById("text1").outerHTML="556 517 54";
+//        document.getElementById("text2").outerHTML="556 518 43";
+//        document.getElementById("text3").outerHTML="070 331 50 43";
+//    }
+//    else if (value == "uppsala") {
+//        document.getElementById("text1").outerHTML="556 517 49";
+//        document.getElementById("text2").outerHTML="556 518 65";
+//        document.getElementById("text3").outerHTML="070 331 54 57";
+//    }
+//}
+
+
+
