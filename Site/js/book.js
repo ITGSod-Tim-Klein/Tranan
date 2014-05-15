b3b9f9d51b23157ddf50d9cff395a7492850d60a
Lambi <tim.kleinbergman@elev.it-gymnasiet.se>
2014-05-15 09:16:24 +0200

    Added clear and booking properties on purchase and cancel button

--- Site/js/book.js
+++ Site/js/book.js
@@ -1,7 +1,7 @@
-var call_times1 = 0
-var call_times2 = 0
-var call_times3 = 0
-var call_times4 = 0
+var call_times1 = 1
+var call_times2 = 1
+var call_times3 = 1
+var call_times4 = 1
 var text1 = 'Three Star Hotel'
 var text2 = 'Two Star Hotel'
 var text3 = 'One Star Hotel'
@@ -26,7 +26,7 @@ function book(text, number) {
 
     var recent = document.getElementById("purchases"+number).innerHTML
     if (document.getElementById("purchases"+number).innerHTML == "") {
-        document.getElementById("purchases"+number).innerHTML= text
+        document.getElementById("purchases"+number).innerHTML= text + "(" + call_times(number) + ")"
     }
 
     else if (recent == (text + "(" + call_times(number) + ")") || recent == text) {
@@ -70,3 +70,28 @@ function add_call_times(number) {
     }
 }
 
+function reset_order() {
+    document.getElementById("purchases1").innerHTML = ""
+    document.getElementById("purchases2").innerHTML = ""
+    document.getElementById("purchases3").innerHTML = ""
+    document.getElementById("purchases4").innerHTML = ""
+    call_times1 = 1
+    call_times2 = 1
+    call_times3 = 1
+    call_times4 = 1
+}
+
+function order_successful() {
+    var purchase1 = document.getElementById("purchases1").innerHTML
+    var purchase2 = document.getElementById("purchases2").innerHTML
+    var purchase3 = document.getElementById("purchases3").innerHTML
+    var purchase4 = document.getElementById("purchases4").innerHTML
+
+    if (purchase1 == "" && purchase2 == "" && purchase3 == "" && purchase4 == "") {
+        alert("Please book something before purchase")
+    }
+    else {
+        reset_order()
+        alert("Booking successful")
+    }
+}
