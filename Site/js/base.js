bbbf7f9541015060f594ac9459ed7ee870f9988a
Lambi <tim.kleinbergman@elev.it-gymnasiet.se>
2014-05-15 12:27:15 +0200

    Enable detection by pressing enter

--- Site/js/base.js
+++ Site/js/base.js
@@ -4,3 +4,12 @@ function auto_search() {
 
     window.open("http://lmgtfy.com/?q="+(res));
 }
+
+function enters(event) {
+    if (event.which == 13 || event.keyCode == 13) {
+        //code to execute here
+        auto_search()
+        return false;
+    }
+    return true;
+}
