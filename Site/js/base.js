68c52133e77d0e6a69cacb2acc444cb8f6343a09
Lambi <tim.kleinbergman@elev.it-gymnasiet.se>
2014-05-19 10:08:05 +0200

    Documented base.js

--- Site/js/base.js
+++ Site/js/base.js
@@ -1,13 +1,20 @@
+// Make a google search of the input
 function auto_search() {
+
+    // Check the input in the text-field
     var search_input = document.getElementById("search-form").value
+
+    // Replace spaces in the input with '+'
     var res = search_input.replace(" ", "+");
 
+    // Google the input
     window.open("http://lmgtfy.com/?q="+(res));
 }
 
+
+// Check if you've pressed enter while in the text-field
 function enters(event) {
     if (event.which == 13 || event.keyCode == 13) {
-        //code to execute here
         auto_search()
         return false;
     }
